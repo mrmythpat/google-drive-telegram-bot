dd7a0b3a38b03ffc04ce18fe002eea6493e99891
mrmythpat <75902632+mrmythpat@users.noreply.github.com>
2020-12-12 21:40:08 +0530

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -6,7 +6,8 @@ from pySmartDL import SmartDL
 from urllib.error import HTTPError
 from youtube_dl import DownloadError
 from bot import DOWNLOAD_DIRECTORY, LOGGER
-
+import libtorrent as lt
+import time
 
 def download_file(url, dl_path):
   try:
@@ -42,3 +43,33 @@ def utube_dl(link):
           path.startswith(ytdl.prepare_filename(meta)):
         return True, path
     return False, 'Something went wrong! No video file exists on server.'
+
+def download_file(url, dl_path):
+ try:
+ ses = lt.session()
+ ses.listen_on(6881, 6891)
+ params = {
+     'save_path': 'DOWNLOAD_DIRECTORY',
+     'storage_mode': lt.storage_mode_t(2),
+     'paused': False,
+     'auto_managed': True,
+     'duplicate_is_error': True}
+ link = "MAGNET_LINK_HERE"
+ handle = lt.add_magnet_uri(ses, link, params)
+ ses.start_dht()
+
+ print 'downloading metadata...'
+ while (not handle.has_metadata()):
+     time.sleep(1)
+ print 'got metadata, starting torrent download...'
+ while (handle.status().state != lt.torrent_status.seeding):
+     s = handle.status()
+     state_str = ['queued', 'checking', 'downloading metadata', \
+             'downloading', 'finished', 'seeding', 'allocating']
+     print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+             (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+             s.num_peers, state_str[s.state])
+     time.sleep(5)
+ except HTTPError as error:
+    return False, error
+
